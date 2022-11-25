ec5bcaf69fc5d5e4a91f64356a976a0061de44f1
Ugur <uguryildiz@aksaray.edu.tr>
2022-11-25 21:19:45 +0300

    cesitli yenilikler

--- src/main/java/Pages/DBQueries.java
+++ src/main/java/Pages/DBQueries.java
@@ -6,11 +6,9 @@ import io.appium.java_client.android.AndroidDriver;
 import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
 import org.json.simple.parser.ParseException;
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.SQLException;
-
 import static Libraries.BaseClass.getString;
 
 /**
@@ -19,13 +17,13 @@ import static Libraries.BaseClass.getString;
 public class DBQueries {
 
 
-    public static final String GET_SEMI_AMOUNT_SQL = "SELECT TOP (1) c.ACCOUNT_BALANCE FROM [TTPAY_TEST].[dbo].[ACC_USER] a, [TTPAY_TEST].[dbo].[EMN_MEMBER] b, [TTPAY_TEST].[dbo].[EMN_MEMBER_DETAIL_LOG] c WHERE a.USER_ID = b.USER_ID AND b.MEMBER_CODE = c.MEMBER_CODE AND a.ACTIVE_MSISDN = '905980953248' ORDER BY c.CREATED_DATE DESC";
+    public static final String GET_SEMI_AMOUNT_SQL = "SELECT TOP (1) c.ACCOUNT_BALANCE FROM [TTPAY_TEST].[dbo].[ACC_USER] a, [TTPAY_TEST].[dbo].[EMN_MEMBER] b, [TTPAY_TEST].[dbo].[EMN_MEMBER_DETAIL_LOG] c WHERE a.USER_ID = b.USER_ID AND b.MEMBER_CODE = c.MEMBER_CODE AND a.ACTIVE_MSISDN = '905051231231' ORDER BY c.CREATED_DATE DESC";
     public static final String SET_SEMIVERIFIED_OTP_SQL;
     public static final String SET_BASIC_OTP_SQL;
     public static final String SET_VERIFIED_OTP_SQL;
     private static final String GET_OTP_SQL = "select TOP(1) TEXT from [TTPAY_TEST].[dbo].[SMS_TRANSACTION] ORDER BY CREATION_DATE DESC";
     private static final String GET_PN_SQL = "SELECT TOP(1) BODY FROM [TTPAY_TEST].[dbo].[PN_TRANSACTION] order by LOG_DATE desc";
-    private static final String GET_TOTAL_TAX_AMOUNT_SEMI = "select top(1) TOTAL_TAX_AMOUNT from PAY_BILL where RECEIVER_EMAIL_ADDRESS in (select ACTIVE_MAIL from ACC_USER where ACTIVE_MSISDN='905980953248')  order by CREATED_DATE desc";
+    private static final String GET_TOTAL_TAX_AMOUNT_SEMI = "select top(1) TOTAL_TAX_AMOUNT from PAY_BILL where RECEIVER_EMAIL_ADDRESS in (select ACTIVE_MAIL from ACC_USER where ACTIVE_MSISDN='905051231231')  order by CREATED_DATE desc";
     private static final String GET_TOTAL_TAX_AMOUNT_VERIFIED = "select top(1) TOTAL_TAX_AMOUNT from [TTPAY_TEST].[dbo].[PAY_BILL] where RECEIVER_EMAIL_ADDRESS in (select ACTIVE_MAIL from [TTPAY_TEST].[dbo].[ACC_USER] where ACTIVE_MSISDN='905997654321')  order by CREATED_DATE desc";
     private static final String GET_BASIC_AMOUNT_SQL = "SELECT TOP (1) c.ACCOUNT_BALANCE FROM [TTPAY_TEST].[dbo].[ACC_USER] a, [TTPAY_TEST].[dbo].[EMN_MEMBER] b, [TTPAY_TEST].[dbo].[EMN_MEMBER_DETAIL_LOG] c WHERE a.USER_ID = b.USER_ID AND b.MEMBER_CODE = c.MEMBER_CODE AND a.ACTIVE_MSISDN = '905550000012' ORDER BY c.CREATED_DATE DESC";
     private static final String GET_VERIFIED_AMOUNT_SQL = "SELECT TOP (1) c.ACCOUNT_BALANCE FROM [TTPAY_TEST].[dbo].[ACC_USER] a, [TTPAY_TEST].[dbo].[EMN_MEMBER] b, [TTPAY_TEST].[dbo].[EMN_MEMBER_DETAIL_LOG] c WHERE a.USER_ID = b.USER_ID AND b.MEMBER_CODE = c.MEMBER_CODE AND a.ACTIVE_MSISDN = '905997654321' ORDER BY c.CREATED_DATE DESC";
