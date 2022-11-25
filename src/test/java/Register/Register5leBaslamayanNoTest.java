96bd898b90f5185f3eb708dbab66c942a0a15b29
Ugur Yildiz <uguryildiz@aksaray.edu.tr>
2022-11-25 14:14:14 +0300

    commit

--- src/test/java/Register/Register5leBaslamayanNoTest.java
+++ src/test/java/Register/Register5leBaslamayanNoTest.java
@@ -14,7 +14,7 @@ public class Register5leBaslamayanNoTest extends BaseClass {
     @Test(priority = 0)
     @Severity(SeverityLevel.MINOR)
     @Step(STEP_MESSAGE)
-    @Description("Sisteme 5 ile başlayan yeni kullanıcı kaydetme senaryosu.")
+    @Description("Sisteme 5 ile başlamayan yeni kullanıcı kaydetme senaryosu.")
     public void Register5ileBaslamayanNo() {
 
         registerPage
